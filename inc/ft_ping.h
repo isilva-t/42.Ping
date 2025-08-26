d687c44c3c43dce7f6fe33ec098aec6c7b61a748
isilva-t <ivanrfteixeira@gmail.com>
2025-08-26 19:59:29 +0100

    8bit struct

--- inc/ft_ping.h
+++ inc/ft_ping.h
@@ -1,3 +1,21 @@
+#ifndef FT_PING_H
+# define FT_PING_H
+
 #include <stdio.h>
 #include <unistd.h>
 #include "../help.h"
+#include <stdint.h>
+
+struct ping_flags {
+   uint8_t verbose	: 1;  // -v flag
+   uint8_t help		: 1;  // -? flag
+   uint8_t flood	: 1;  // -f flag (bonus)
+   uint8_t numeric	: 1;  // -n flag (bonus)
+   uint8_t quiet	: 1;  // -q flag (bonus)
+   uint8_t timestamp	: 1;  // -T flag (bonus)
+   uint8_t res1		: 1;
+   uint8_t res2		: 1;
+} __attribute__((packed));
+
+
+#endif
